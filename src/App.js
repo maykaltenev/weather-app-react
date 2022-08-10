7259cfa1aace821bb29142cbf795e4590e0ede86
maykaltenev <michaeltenev@gmail.com>
2022-08-11 00:46:26 +0200

    weather starter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
-
+  const [city, setCity] = useState('Boston');
+  const [data, setData] = useState('');
+  const [search, setSearch] = useState('');
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    setCity(data)
+    console.log(data)
+  }
   useEffect(() => {
-    fetch("http://www.omdbapi.com/?apikey=d9983ef2")
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2464cea4299cf8c159463e039edc6cb6&units=metric`)
       .then(response => response.json())
-      .then(response => console.log(response))
+      .then(data => setSearch(data))
+
       .catch(err => console.error(err));
-  }, []);
+  }, [city]);
 
   return (
     <div className="App">
-
+      <form >
+        <input type='search' name='search' onChange={(e) => setData(e.target.value)} />
+        <button onClick={onClickHandler}>Search</button>
+      </form>
+      <div>The city is: {search?.name}</div>
+      <div>Temperature is {search.main?.temp.toFixed(0)}</div>
     </div>
   );
 }
