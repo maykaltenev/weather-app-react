8ecf2a67d1effc25a22ff2480f5cbb8402b0d3b0
Maykal Tenev <begemoth@begem0th.fritz.box>
2022-08-16 19:13:03 +0200

    style change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import { useEffect, useContext, useState } from "react";
-import usePrevious from "./hooks/usePrevious";
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { Marker, Popup } from "react-leaflet";
@@ -16,28 +15,7 @@ import AsideBar from "./components/AsideBar";
 import Future from "./components/Future";
 
 function App() {
-  // function currentTime() {
-  //   const date = new Date(); /* creating object of Date class */
-  //   let hour = date.getHours();
-  //   let min = date.getMinutes();
-  //   let sec = date.getSeconds();
-  //   hour = updateTime(hour);
-  //   min = updateTime(min);
-  //   sec = updateTime(sec);
-  //   let clock = hour + " : " + min + " : " + sec; /* adding time to the div */
-  //   let t = setTimeout(function () { currentTime() }, 1000); /* setting timer */
-  // }
-  // function updateTime(k) {
-  //   if (k < 10) {
-  //     return "0" + k;
-  //   }
-  //   else {
-  //     return k;
-  //   }
-  // }
-  const [newMap, setNewMap] = useState(false);
-  const { fiveDays, future, dayOrNight, conditions, city, setSearch, position, search } = useContext(DataContext);
-  const previous = usePrevious(city)
+  const { fiveDays, future, dayOrNight, conditions, city, setSearch, search } = useContext(DataContext);
   useEffect(() => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2464cea4299cf8c159463e039edc6cb6&units=metric`)
       .then(response => response.json())
@@ -45,14 +23,6 @@ function App() {
       .catch(err => console.error(err));
   }, [city]);
 
-  // useEffect(() => {
-  //   if (previous !== city) {
-  //     setNewMap(false);
-  //   }
-  //   console.log(newMap);
-  //   console.log(city);
-  //   console.log(previous)
-  // }, [city, previous])
   return (
     <div className={conditions && dayOrNight ? `${conditions} ${dayOrNight}` : 'wrapper'
     }>
